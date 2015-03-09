4575e985468a2b0499d40550df9b94c414d83e1b
Eric Kindler <ericthekindler@Erics-MacBook-Pro.local>
2015-03-09 17:00:19 -0400

    remove placeholder LI.

--- js/listly.js
+++ js/listly.js
@@ -1,10 +1,19 @@
-function Listly() {
-    function Listly() {
-      this.task = [];
+var Listly = function() {
 
-      var self = this;
-    }
-    return Listly;
-}
+  function Listly() {
+    this.tasks = [];
+    var self = this;
+
+    $('form#new_task').submit(function(ev) {
+      ev.preventDefault();
+      var task_name = this.task_name.value;
+      var result = $('#tasks').append('<li class="list-group-item">' + task_name + '</li>');
+      this.task_name.value ='';
+      $(this.task_name);
+    });
+  }
+
+  return Listly;
+}();
 
 var listly = new Listly();
