6b04cd66779e35d76bde81a186c904d8a72e8201
Eric Kindler <ericthekindler@Erics-MacBook-Pro.local>
2015-03-10 16:13:38 -0400

    "Detete tasks from local storage"

---
+++ js/task.js
@@ -0,0 +1,14 @@
+var Task = function() {
+  function Task() {
+    var self = this;
+
+    function Task(name) {
+      this.name = name;
+    }
+  }
+  return Task;
+}();
+
+var task_name = 'Task 1';
+
+var my_task = new Task(task_name);
