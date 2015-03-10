4281fe1c2a8bbd8f902f719de19ed5198f235ead
Eric Kindler <ericthekindler@Erics-MacBook-Pro.local>
2015-03-09 20:45:14 -0400

    Save and load tasks to/from localStorage.

--- js/listly.js
+++ js/listly.js
@@ -1,15 +1,47 @@
 var Listly = function() {
 
   function Listly() {
-    this.tasks = [];
     var self = this;
+    self.tasks = [];
 
-    $('form#new_task').submit(function(ev) {
+    function addTask(task_name) {
+      self.tasks.push(task_name);
+      if (save()) {
+        $('#tasks').append('<li class="list-group-item">' + task_name + '</li>');
+        return true;
+      }
+      else {
+        return false;
+      }
+    }
+
+    function load() {
+      self.tasks = JSON.parse(localStorage.tasks);
+      $.each(self.tasks, function(index, task_name) {
+        $('#tasks').append('<li class="list-group-item">' + task_name + '</li>');
+      });
+    }
+
+    function save() {
+      try {
+        return (localStorage.tasks = JSON.stringify(self.tasks));
+      }
+      catch(err) {
+        return false;
+      }
+    }
+
+    load();
+
+    $('form#new_task').on('submit', function(ev) {
       ev.preventDefault();
-      var task_name = this.task_name.value;
-      var result = $('#tasks').append('<li class="list-group-item">' + task_name + '</li>');
-      this.task_name.value ='';
-      $(this.task_name);
+      var field = $(this.task_name);
+      var task_name = field.val();
+
+      if (addTask(task_name)) {
+        field.val('');
+      }
+      field.focus().select();
     });
   }
 
